0fe5253a20729d9b92e470d536021815bb8b70ee
ollabu3 <ollabu3@gmail.com>
2024-01-14 14:38:09 +0900

    tooltip 마우스 우상단 위치 조정

--- components/common/Tooltip.tsx
+++ components/common/Tooltip.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from "react";
 import styles from "../../styles/common/tooltip.module.css";
 
-// 툴팁은 마우스 포인터 우상단에 위치 하며, 마우스의 움직임을 따라 지속적으로 포지션이 업데이트 되어야 함
 export default function Tooltip() {
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const getCurrentPosition = (e: MouseEvent) => {
+      const currentTooltip = tooltipRef.current;
+
+      if (currentTooltip) {
+        const tooltipHeight = currentTooltip.offsetHeight; // 툴팁의 height 사이즈
+        const x = e.clientX;
+        const y = e.clientY - tooltipHeight; // 현재 마우스 위치에서  - height 사이즈
+        currentTooltip.style.left = x + "px";
+        currentTooltip.style.top = y + "px";
+      }
+    };
+
+    window.addEventListener("mousemove", getCurrentPosition);
+    return () => {
+      window.removeEventListener("mousemove", getCurrentPosition);
+    };
+  }, []);
+
   return (
-    <div className={styles.tooltip}>
+    <div ref={tooltipRef} className={styles.tooltip}>
       <p className={styles.text}>tooltip</p>
     </div>
   );
